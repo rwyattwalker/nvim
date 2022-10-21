1576782792067411e2a897b1226cb648891a2fc5
Wyatt Walker <rwyattwalker@gmail.com>
2022-10-21 10:51:32 -0500

    Updated Neo-Tree Config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -129,7 +129,15 @@ return require('packer').startup(function(use)
       "MunifTanjim/nui.nvim",
     }
   }
-
+  require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = false,
+    }}
+  })
+  -- unrelated
   -- Tim Pope Plugins --
   use 'tpope/vim-surround'
 
